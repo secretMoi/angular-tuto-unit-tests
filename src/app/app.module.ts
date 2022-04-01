c4e3895d0672741656eb94513617fd19078be105
secretMoi <win.moi@hotmail.com>
2022-04-01 12:30:06 +0200

    Testing Dependencies: Components and Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user/user.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
